509999b50c5a70b50d5c0e711bf530a988aade8b
Mani Kant Sharma <manikant.sharma@ampleserv.com>
2025-08-12 11:43:10 +0530

    Prisma Generate

--- src/v1/routes/user.routes.ts
+++ src/v1/routes/user.routes.ts
@@ -4,7 +4,6 @@ import {
   deleteUser,
   getUser,
   getUsersList,
-  getUserStats,
   updateUser,
 } from "../controllers/user.controller";
 import { authenticateToken } from "../../middlewares/auth";
@@ -13,9 +12,6 @@ const routes = Router();
 
 routes.post("/users", authenticateToken, (req, res) => createUser(req, res));
 routes.get("/users", authenticateToken, (req, res) => getUsersList(req, res));
-routes.get("/users/stats", authenticateToken, (req, res) =>
-  getUserStats(req, res)
-);
 routes.get("/users/:id", authenticateToken, (req, res) => getUser(req, res));
 routes.put("/users/:id", authenticateToken, (req, res) => updateUser(req, res));
 routes.delete("/users/:id", authenticateToken, (req, res) =>
