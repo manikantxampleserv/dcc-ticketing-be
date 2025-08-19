5adb6691ffdd4849ef28f4e15edbb41bd43fea27
Mani Kant Sharma <manikant.sharma@ampleserv.com>
2025-08-19 16:25:49 +0530

    mkx

--- src/utils/prisma.config.ts
+++ src/utils/prisma.config.ts
@@ -2,25 +2,6 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient({
   log: ["query", "info", "warn", "error"],
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL,
-    },
-  },
-});
-
-process.on("beforeExit", async () => {
-  await prisma.$disconnect();
-});
-
-process.on("SIGINT", async () => {
-  await prisma.$disconnect();
-  process.exit(0);
-});
-
-process.on("SIGTERM", async () => {
-  await prisma.$disconnect();
-  process.exit(0);
 });
 
 export default prisma;
